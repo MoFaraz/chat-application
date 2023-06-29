d21d5ad9188bd14ee9d3665430e340d0124607fb
mopharaz <mopharaz@gmail.com>
2023-06-29 08:02:08 +0330

    move react to client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import ToggleColorMode from "./components/ToggleColorMode"
-
+import Views from "./components/Views";
 function App() {
   return (
     <>
+      <Views/>
       <ToggleColorMode />
     </>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from "react"
 import App from './App';
 import ReactDOM from "react-dom";
 import theme from "./theme";
+import { BrowserRouter } from "react-router-dom";
 import {ColorModeScript} from "@chakra-ui/color-mode";
 import {ChakraProvider} from "@chakra-ui/react"
 
 
 ReactDOM.render(
 <React.StrictMode>
+    <BrowserRouter>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root"));
