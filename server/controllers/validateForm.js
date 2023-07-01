a3181affaeff80e7d36eb71b2a75db4bff458de6
mopharaz <mopharaz@gmail.com>
2023-07-01 05:15:40 +0330

    update

--- server/controllers/validateForm.js
+++ server/controllers/validateForm.js
@@ -1,16 +1,4 @@
-const Yup = require("yup");
-
-const formSchema = Yup.object({
-    username: Yup.string()
-        .required("Username required")
-        .min(6, "Username too short")
-        .max(28, "Username too long!"),
-    password: Yup.string()
-        .required("Password required")
-        .min(6, "Password too short")
-        .max(28, "Password too long!"),
-});
-
+const {formSchema} = require("@whatsapp-clone/common-validate")
 const validateForm = (req, res) => {
     const formData = req.body;
     formSchema
