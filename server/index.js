467990c564a83ddf0d82cd8214715057a5ad81b7
mopharaz <mopharaz@gmail.com>
2023-07-04 08:19:41 +0330

    just clean index.js

--- server/index.js
+++ server/index.js
@@ -1,49 +1,28 @@
 const express = require("express");
 const { Server } = require("socket.io");
+const {sessionMiddleware, wrap, corsConfig} = require('./controllers/serverController')
 const app = express();
 const helmet = require("helmet");
 const cors = require("cors");
 const authRouter = require("./routers/authRouter");
-const session = require("express-session");
 const server = require("http").createServer(app);
 const redisClient = require("./redis");
-const RedisStore = require('connect-redis')(session)
-
-
 require("dotenv").config();
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        credentials: "true",
-    },
+    cors: corsConfig
 });
 app.use(helmet());
 app.use(
-    cors({
-        origin: "http://localhost:3000",
-        credentials: true,
-    })
+    cors(corsConfig)
 );
 app.use(express.json());
-app.use(
-    session({
-        secret: process.env.COOKIE_SECRET,
-        credentials: true,
-        name: "sid",
-        store: new RedisStore({ client: redisClient }),
-        resave: false,
-        saveUninitialized: false,
-        cookie: {
-            secure: process.env.ENVIRONMENT === "production" ? "true" : "auto",
-            httpOnly: true,
-            expires: 1000 * 60 * 60 * 24 * 7,
-            sameSite: process.env.ENVIRONMENT === "production" ? "none" : "lax",
-        },
-    })
-);
+app.use(sessionMiddleware);
 app.use("/auth", authRouter);
 
-io.on("connect", socket => {});
+io.use(wrap(sessionMiddleware));
+io.on("connect", socket => {
+    console.log(socket.request.session.user.username)
+});
 
 server.listen(4000, () => {
     console.log("Server listening on port 4000");
